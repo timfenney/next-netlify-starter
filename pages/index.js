f332f9d7fd671661350ff88df6f06e9e82bd9f55
Timothy Fenney <timothy@viafoura.com>
2021-01-25 11:29:44 -0500

    Add example <amp-iframe />.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { useAmp } from 'next/amp'
 
 export default function Home() {
+  let isAmp = useAmp()
   return (
     <div className="container">
       <Head>
@@ -15,6 +17,15 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <amp-iframe
+          width="200"
+          height="100"
+          sandbox="allow-scripts allow-same-origin"
+          layout="responsive"
+          frameborder="0"
+          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAyAS599A2GGPKTmtNr9CptD61LE4gN6oQ&q=iceland"
+        >
+        </amp-iframe>
       </main>
 
       <Footer />
